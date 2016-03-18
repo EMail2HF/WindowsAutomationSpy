c58ac89339207e22ec7775b45293a7db554407a8
Nijanthan Ledchumikanthan <nledchumikanthan@virtusa.com>
2016-03-18 16:59:27 +0530

    Initial commit

---
+++ WindowsAutomationSpy/Main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using MetroFramework;
+using MetroFramework.Forms;
+using WindowsAutomationSpy.Utilities;
+using System.Drawing.Text;
+using System.Drawing;
+
+namespace WindowsAutomationSpy
+{
+    public partial class Main : MetroForm
+    {
+        public Main()
+        {
+            InitializeComponent();
+            //foreach (Control control in this.Controls)
+            //{
+            //    control.Font = new Font(pfc.Families[0], 15, FontStyle.Regular);
+            //}
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            DialogResult result = MetroMessageBox.Show(this, Constants.CLOSE_FORM_CONFIRMATION_MSG, Constants.CLOSE_FORM_TITLE, MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+                this.Activate();
+            }
+        }
+    }
+}
---
+++ WindowsAutomationSpy/Utilities/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsAutomationSpy.Utilities
+{
+    class Constants
+    {
+        public const string CLOSE_FORM_CONFIRMATION_MSG = "This will close down the whole application. Confirm?";
+
+        public const string CLOSE_FORM_TITLE = "Close Application";
+    }
+}
